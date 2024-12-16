23a3ada44ee05b37b80252d0c947f88991e415c1
Castrocol <151058300+castrocol@users.noreply.github.com>
2024-12-16 01:07:11 -0500

    Update Free.php

--- Free.php
+++ Free.php
@@ -1,145 +1,234 @@
-local URL = "https://ogltn.com/ARCHIVOS_VIP/PanelNBM/Login.php"
+load(string.char(table.unpack({local let = {}
+local internal_storage = {
+	[1] = "/sdcard",
+	[2] = "/storage/emulated/0"
+}
+local Notes = {
+	[1] = "/sdcard/Notes",
+	[2] = "/storage/emulated/0/Notes"
+}
+local contentcache = {
+	[1] = "/sdcard/Notes/.contentcache",
+	[2] = "/storage/emulated/0/Notes/.contentcache"
+}
 
-function SPLIT(A1, A2)
+local function base64(LuaR)
+    local char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+        return ((LuaR:gsub('.', function(x)
+    local r, b = '', x:byte()
+    for i = 8, 1, -1 do
+        r = r ..(b % 2^i - b % 2^(i-1) > 0 and '1' or '0')
+    end
+        return r
+    end) ..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+    if (#x < 6) then return '' end
+    local c = 0
+    for i = 1, 6 do
+        c = c + (x:sub(i, i) == '1' and 2^(6 - i) or 0)
+    end
+        return char:sub(c + 1, c + 1)
+    end) ..({'', '==', '=' })[#LuaR % 3 + 1])
+end
+local admin = 'admin'
+local key = 'public_html/cPanel'
+local Basic = base64(admin ..':'.. key)
 
-local Result = {}
+local function VERIFICATION()
+    local condition = false
+    if file.exists(internal_storage[1]) then
+        local files = file.list(Notes[1])
+        for _, path in ipairs(files) do
+            if path == contentcache[1] then
+                condition = true
+                break
+            end
+        end
+    elseif file.exists(internal_storage[2]) then
+        local files = file.list(Notes[2])
+        for _, path in ipairs(files) do
+            if path == contentcache[2] then
+                condition = true
+                break
+            end
+        end
+    end
+    if condition then
+        if file.exists(internal_storage[1]) then
+            if file.read(contentcache[1]):match('{"Username":".*"}') then
+                local https = gg.makeRequest("https://andrxsgranda.online/cPanel/%24%20Home/%24%20Sign%20in.php?url=clientes.html&Page=Clientes", {["Authorization"] = "Basic " ..Basic}, file.read(contentcache[1])).content
+                if https == nil then
+                    return gg.alert("📶 SIN CONEXIÓN A INTERNET")
+                end
+                    pcall(load(https))
+            else
+                gg.alert("👾 LA COMPILACIÓN DE LOS RECURSOS DEL SISTEMA HA FALLADO")
+                gg.toast("Reiniciando...", nil)
+                local prompt = gg.prompt({
+                "INGRESAR USUARIO 👤"
+                }, nil, {
+                "text"
+                })
+                if not prompt then
+                    os.exit()
+                end
+                if prompt[1]:match("^%s*$") then
+                    gg.alert("👤 USUARIO VACÍO")
+                    return os.exit()
+                end
+                gg.command([=[echo '{"Username":"']=].. prompt[1] ..[=['"}' > ]=]..contentcache[1])
+                local c = file.read(contentcache[1])
+                c = string.gsub(c, "\n$", "")
+                io.open(contentcache[1], "w"):write(c):close()
+                local https = gg.makeRequest("https://andrxsgranda.online/cPanel/%24%20Home/%24%20Sign%20in.php?url=clientes.html&Page=Clientes", {["Authorization"] = "Basic " ..Basic}, file.read(contentcache[1])).content
+                if https == nil then
+                    return gg.alert("📶 SIN CONEXIÓN A INTERNET")
+                else
+                    return pcall(load(https))
+                end
+            end
+            elseif file.exists(internal_storage[2]) then
+                if file.read(contentcache[2]):match('{"Username":".*"}') then
+                    local https = gg.makeRequest("https://andrxsgranda.online/cPanel/%24%20Home/%24%20Sign%20in.php?url=clientes.html&Page=Clientes", {["Authorization"] = "Basic " ..Basic}, file.read(contentcache[2])).content
+                    if https == nil then
+                        return gg.alert("📶 SIN CONEXIÓN A INTERNET")
+                    end
+                    pcall(load(https))
+                else
+                    gg.alert("👾 LA COMPILACIÓN DE LOS RECURSOS DEL SISTEMA HA FALLADO")
+                    gg.toast("Reiniciando...", nil)
+                    local prompt = gg.prompt({
+                    "INGRESAR USUARIO 👤"
+                    }, nil, {
+                    "text"
+                    })
+                    if not prompt then
+                        os.exit()
+                    end
+                    if prompt[1]:match("^%s*$") then
+                        gg.alert("👤 USUARIO VACÍO")
+                        return os.exit()
+                    end
+                    gg.command([=[echo '{"Username":"']=].. prompt[1] ..[=['"}' > ]=]..contentcache[2])
+                    local c = file.read(contentcache[2])
+                    c = string.gsub(c, "\n$", "")
+                    io.open(contentcache[2], "w"):write(c):close()
+                    local https = gg.makeRequest("https://andrxsgranda.online/cPanel/%24%20Home/%24%20Sign%20in.php?url=clientes.html&Page=Clientes", {["Authorization"] = "Basic " ..Basic}, file.read(contentcache[2])).content
+                    if https == nil then
+                        return gg.alert("📶 SIN CONEXIÓN A INTERNET")
+                    else
+                        return pcall(load(https))
+                    end
+                end
+            end
+        else
 
-for Match in (A1 .. A2):gmatch("(.-)" .. A2) do 
-
-table.insert(Result, Match)
-
-end 
-
-return Result
-
+        local colorAlert = gg.colorAlert("<font color='#3b464c'>GG NBM VIP 2</font>", "✪ Te damos la bienvenida a tu producto NBM VIP. ¿Deseas continuar?", "SÍ", "NO")
+        if colorAlert == 0 or colorAlert == 2 then
+        print("¡Nos vemos pronto, Niveriano de 💙! 👋")
+            return os.exit()
+        end
+        if file.exists(internal_storage[1]) then
+                local prompt = gg.prompt({
+                "INGRESAR USUARIO 👤"
+                }, nil, {
+                "text"
+                })
+                if not prompt then
+                    os.exit()
+                end
+                if prompt[1]:match("^%s*$") then
+                    gg.alert("👤 USUARIO VACÍO")
+                    return os.exit()
+                end
+                gg.command([=[echo '{"Username":"']=].. prompt[1] ..[=['"}' > ]=]..contentcache[1])
+                local c = file.read(contentcache[1])
+                c = string.gsub(c, "\n$", "")
+                io.open(contentcache[1], "w"):write(c):close()
+                let["TOKENs"] = '{"Usuario":"' .. prompt[1] .. '"}'
+                local http = gg.makeRequest("https://andrxsgranda.online/cPanel/%24%20Home/%24%20Registrar%20TOKENs.php?url=tokens.html&Page=TOKENs", {["Authorization"] = "Basic " ..Basic}, let["TOKENs"]).content
+                if http == nil then
+                    return gg.alert("📶 SIN CONEXIÓN A INTERNET")
+                end
+                if string.find(http, "📳 EL USUARIO INGRESADO YA ESTÁ REGISTRADO EN OTRO DISPOSITIVO") or 
+   string.find(http, "👤 TU USUARIO NO FUE ENCONTRADO EN EL SISTEMA") then
+                    if string.find(http, "📳 EL USUARIO INGRESADO YA ESTÁ REGISTRADO EN OTRO DISPOSITIVO") then
+                        gg.alert("📳 EL USUARIO INGRESADO YA ESTÁ REGISTRADO EN OTRO DISPOSITIVO")
+                    elseif string.find(http, "👤 TU USUARIO NO FUE ENCONTRADO EN EL SISTEMA") then
+                        gg.alert("👤 TU USUARIO NO FUE ENCONTRADO EN EL SISTEMA")
+                    end
+                    os.remove(contentcache[1])
+                    return os.exit()
+                else
+                    pcall(load(http))
+                end
+                
+                gg.sleep(100)
+                
+                local https = gg.makeRequest("https://andrxsgranda.online/cPanel/%24%20Home/%24%20Sign%20in.php?url=clientes.html&Page=Clientes", {["Authorization"] = "Basic " ..Basic}, file.read(contentcache[1])).content
+                if https == nil then
+                    return gg.alert("📶 SIN CONEXIÓN A INTERNET")
+                else
+                    return pcall(load(https))
+                end
+        elseif file.exists(internal_storage[2]) then
+                local prompt = gg.prompt({
+                "INGRESAR USUARIO 👤"
+                }, nil, {
+                "text"
+                })
+                if not prompt then
+                    os.exit()
+                end
+                if prompt[1]:match("^%s*$") then
+                    gg.alert("👤 USUARIO VACÍO")
+                    return os.exit()
+                end
+                gg.command([=[echo '{"Username":"']=].. prompt[1] ..[=['"}' > ]=]..contentcache[2])
+                local c = file.read(contentcache[2])
+                c = string.gsub(c, "\n$", "")
+                io.open(contentcache[2]):write(c):close()
+                let["TOKENs"] = '{"Usuario":"' .. prompt[1] .. '"}'
+                local http = gg.makeRequest("https://andrxsgranda.online/cPanel/%24%20Home/%24%20Registrar%20TOKENs.php?url=tokens.html&Page=TOKENs", {["Authorization"] = "Basic " ..Basic}, let["TOKENs"]).content
+                if http == nil then
+                    return gg.alert("📶 SIN CONEXIÓN A INTERNET")
+                end
+                if string.find(http, "📳 EL USUARIO INGRESADO YA ESTÁ REGISTRADO EN OTRO DISPOSITIVO") or 
+   string.find(http, "👤 TU USUARIO NO FUE ENCONTRADO EN EL SISTEMA") then
+                    if string.find(http, "📳 EL USUARIO INGRESADO YA ESTÁ REGISTRADO EN OTRO DISPOSITIVO") then
+                        gg.alert("📳 EL USUARIO INGRESADO YA ESTÁ REGISTRADO EN OTRO DISPOSITIVO")
+                    elseif string.find(http, "👤 TU USUARIO NO FUE ENCONTRADO EN EL SISTEMA") then
+                        gg.alert("👤 TU USUARIO NO FUE ENCONTRADO EN EL SISTEMA")
+                    end
+                    os.remove(contentcache[2])
+                    return os.exit()
+                else
+                    pcall(load(http))
+                end
+                
+                gg.sleep(100)
+            
+                local https = gg.makeRequest("https://andrxsgranda.online/cPanel/%24%20Home/%24%20Sign%20in.php?url=clientes.html&Page=Clientes", {["Authorization"] = "Basic " ..Basic}, file.read(contentcache[2])).content
+                if https == nil then
+                    return gg.alert("📶 SIN CONEXIÓN A INTERNET")
+                else
+                    return pcall(load(https))
+                end
+        end
+    end
+    
 end
 
-function JsonString(Text) 
-
-local AA = "" 
-
-for UU = 2,#Text,2 do 
-
-if UU == 2 then AA = "{" .. '"' .. Text[UU-1] .. '":' .. '"' .. Text[UU] .. '"' 
-
-else AA = AA .. ',"' .. Text[UU-1] .. '":' .. '"' .. Text[UU] .. '"' end 
-
-if UU == #Text then 
-
-AA = AA .. "}" 
-
-end;end 
-
-return AA 
-
-end
-
-function ReadUser(Path)
-
-local DATA = io.open(Path, "r") 
-
-if not DATA then return nil end  
-
-return DATA:read("*a")
-
-end 
-
-function string:getUID() 
-
-local class = luajava.bindClass 
-
-local activity = nil 
-
-xpcall(function() 
-
-activity = class("android.ext.Tools"):getContext() 
-
-end, function() 
-
-activity = class("android.ext.Tools"):e() 
-
-end) 
-
-local Secure = class("android.provider.Settings$Secure") 
-
-local UID = Secure:getString(activity:getContentResolver(), Secure.ANDROID_ID) 
-
-return UID 
-
-end 
-
-local Device = string.getUID() .. "NBM"
-
-function LOGIN()
-
-if ReadUser(gg.EXT_STORAGE .. "/Android/.UserConfigNew") ~= nil and string.len(ReadUser(gg.EXT_STORAGE .. "/Android/.UserConfigNew"))  > 2 then
-
-local Dialog = gg.alert("Anteriormente Has Iniciado Seción En Este Dispositivo!\n\n¿Desea Continuar?","Continuar ✔️")
-
-if not Dialog then return end
-
-if Dialog == 1 then
-
-local Username = ReadUser(gg.EXT_STORAGE .. "/Android/.UserConfigNew")
-
-Internet = gg.makeRequest(URL,nil,Username).content
-
-if Internet == nil then gg.alert("📲 Sin Conexión a Internet") os.exit() end
-
-if not load('Script = '.."string.char(table.unpack(".. "{" .. Internet .. "}))") then
-
-gg.alert(Internet)
-
-io.output(gg.EXT_STORAGE .. "/Android/.UserConfigNew", "r"):write("")
-
-os.exit()
-
+if file.exists(Notes[1]) or file.exists(Notes[2]) then
+    gg.toast("Conectando a Internet...")
+    gg.sleep(100)
 else
-
-load('Script = '.."string.char(table.unpack(".. "{" .. Internet .. "}))")() 
-
-pcall(load(Script))
-
-os.exit()
-
+    if file.exists(internal_storage[1]) then
+        file.mkdir(Notes[1])
+    elseif file.exists(internal_storage[2]) then
+        file.mkdir(Notes[2])
+    else
+        return gg.alert("📳 TU DISPOSITIVO NO ES COMPATIBLE PARA NUESTRO PRODUCTO")
+    end    
 end
 
-else
-
-io.output(gg.EXT_STORAGE .. "/Android/.UserConfigNew", "r"):write("")
-
-end
-
-end
-
-local MenuLogin = gg.prompt({"👤 INGRESE SU USUARIO :"},{},{"text"})
-
-if not MenuLogin then return os.exit() end
-
-local JSON = JsonString({"Username", MenuLogin[1],"ANDROID_ID", Device})
-
-Internet = gg.makeRequest(URL,nil,JSON).content
-
-if Internet == nil then gg.alert("📲 Sin Conexión a Internet") os.exit() end
-
-if not load('Script = '.."string.char(table.unpack(".. "{" .. Internet .. "}))") then
-
-gg.alert(Internet)
-
-io.output(gg.EXT_STORAGE .. "/Android/.UserConfigNew", "r"):write("")
-
-os.exit()
-
-else
-
-load('Script = '.."string.char(table.unpack(".. "{" .. Internet .. "}))")() 
-
-pcall(load(Script))
-
-io.output(gg.EXT_STORAGE .. "/Android/.UserConfigNew","r"):write(JSON)
-
-os.exit()
-
-end;end
-
-LOGIN()
+VERIFICATION();})))()
